22debd66545ca9f97816575bbcb70067ce4c4faa
Jigyasa <114688341+jigyasaba@users.noreply.github.com>
2025-03-01 10:20:57 +0000

    js ecma

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,8 @@
 console.log("Jigyasa  bamola")
+const name="jigyasa"
+let a=10;
+let b;//undefined
+var state="uttarakhand"
+city="dehradun"//reserving space for variable
+console.table([name,a,state,b]);
+/*prefer not to use var because of issue in block scope and function scope*/
---
+++ basics/test2.js
@@ -0,0 +1,11 @@
+"use strict"//treat all js code with newer version
+//alert(3+3)//we are not using nodejs,not  browser 
+//ECMAScript tc39 javascript standardsecm
+//data types in js
+//number 2 to power 53
+//bigint
+let state=null//null is a standalone value where as undefined mean value has not been assigned
+//symbol to find uniqueness odf a component
+//undefined
+console.log(typeof(null))//object
+console.log(typeof(undefined))//undefined
